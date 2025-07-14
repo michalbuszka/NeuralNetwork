445d5527a770dffc1844703e4077b9fcfa42206a
michalbuszka <318049@stud.umk.pl>
2025-07-15 01:31:07 +0200

    Init

---
+++ NeuralNetwork/NeuralNetwork/CanvasController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace NeuralNetwork
+{
+    public class CanvasController
+    {
+        private Canvas canvas; 
+        public CanvasController(Canvas canvas) 
+        {
+            this.canvas = canvas;
+        }
+    }
+}
